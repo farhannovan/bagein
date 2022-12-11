c264031a254af49d72c3345f76fb897eb52af8b2
farhannovan <farhannovan@gmail.com>
2022-12-11 10:03:07 +0700

    add sign

---
+++ app/src/main/java/com/phyxel/bagein/sign/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.phyxel.bagein.sign.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.phyxel.bagein.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
