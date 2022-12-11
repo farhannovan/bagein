78ad717a0deed987ba52d52e532821311d881868
farhannovan <farhannovan@gmail.com>
2022-12-11 14:12:54 +0700

    edit sign

--- app/src/main/java/com/phyxel/bagein/sign/signin/SignInActivity.kt
+++ app/src/main/java/com/phyxel/bagein/sign/signin/SignInActivity.kt
@@ -1,13 +1,39 @@
 package com.phyxel.bagein.sign.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.phyxel.bagein.R
+import com.phyxel.bagein.donasi.DonasiActivity
+import com.phyxel.bagein.sign.signup.SignUpActivity
 
-class SignInActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+//class SignInActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_sign_in)
+//
+//    }
+//}
+
+class SignInActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        val btnRegis = findViewById<Button>(R.id.btnRegis)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnRegis.setOnClickListener {
+            val i = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(i)
+        }
+        btnLogin.setOnClickListener {
+            val i = Intent(this@SignInActivity, DonasiActivity::class.java)
+            startActivity(i)
+        }
     }
 }
