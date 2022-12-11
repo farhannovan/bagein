733cba2ff9f444bd5e9edebba258e8b6548f7365
farhannovan <farhannovan@gmail.com>
2022-12-11 10:55:12 +0700

    add buat donasi form

---
+++ app/src/main/java/com/phyxel/bagein/buatdonasi/BuatDonasiFormActivity.kt
@@ -0,0 +1,11 @@
+package com.phyxel.bagein.buatdonasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuatDonasiFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buat_donasi_form)
+    }
+}
