94d737d75b77e7aabb1d6fa37aad5dd40bc4fc2e
farhannovan <farhannovan@gmail.com>
2022-12-14 10:38:24 +0700

    edit all files

---
+++ app/src/main/java/com/phyxel/bagein/buatdonasi/BuatDonasiActivity.kt
@@ -0,0 +1,12 @@
+package com.phyxel.bagein.buatdonasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.phyxel.bagein.R
+
+class BuatDonasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buat_donasi)
+    }
+}
