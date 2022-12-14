b164534f9f5fd72b6df7773463cb93e14de1c7a7
farhannovan <farhannovan@gmail.com>
2022-12-14 10:57:47 +0700

    add akun detail

---
+++ app/src/main/java/com/phyxel/bagein/akun/AkunDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.phyxel.bagein.akun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.phyxel.bagein.R
+
+class AkunDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_akun_detail)
+    }
+}
