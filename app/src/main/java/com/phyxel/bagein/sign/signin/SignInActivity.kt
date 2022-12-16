afe5ca181be949ffcf529de8451fc2db3207de27
farhannovan <farhannovan@gmail.com>
2022-12-16 08:15:09 +0700

    edit auth

--- app/src/main/java/com/phyxel/bagein/sign/signin/SignInActivity.kt
+++ app/src/main/java/com/phyxel/bagein/sign/signin/SignInActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import com.phyxel.bagein.MainActivity
 import com.phyxel.bagein.R
 import com.phyxel.bagein.sign.signup.SignUpActivity
@@ -24,6 +25,9 @@ class SignInActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
         val btnRegis = findViewById<Button>(R.id.btnRegis)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
 
@@ -35,5 +39,6 @@ class SignInActivity : AppCompatActivity()
             val i = Intent(this@SignInActivity, MainActivity::class.java)
             startActivity(i)
         }
+        
     }
 }
